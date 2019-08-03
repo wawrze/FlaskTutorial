4f9b2c1c4fd9b4cd7763ff4ecffd55d2457cdfbd
Mateusz Wawreszuk <mateusz.wawreszuk@gmail.com>
2019-08-03 21:24:41 +0200

    unit tests

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -32,32 +32,30 @@ def load_logged_in_user():
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
-    username = None
-    password = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
 
-    if username == None:
-        error = 'Username is required.'
-    elif password == None:
-        error = 'Password is required.'
-    elif db.execute(
-        'SELECT id FROM user WHERE username = ?', (username,)
-    ).fetchone() is not None:
-        error = 'User {} is already registered.'.format(username)
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
 
-    if error is None:
-        db.execute(
-            'INSERT INTO user (username, password) VALUES (?, ?)',
-            (username, generate_password_hash(password))
-        )
-        db.commit()
-        return redirect(url_for('auth.login'))
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
 
-    flash(error)
+        flash(error)
 
     return render_template('auth/register.html')
 
